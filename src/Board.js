e7744ec075b0dd4905aa4465b6394de2e912f3b0
marci.mcclenon <marci.mcclenon@company-corp.com>
2020-04-18 23:15:47 -0500

    fix styles

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import './App.css';
-
 import { createBoard, updateClickedStatus, resetBoard } from './helpers.js';
 import Cell from './Cell.js';
 
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import './App.css';
-
 import Board from './Board.js';
 
 class App extends React.Component {
